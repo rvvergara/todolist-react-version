371b1a893e8ec6ead1319fd75b5a15a652e9fe25
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-02-21 18:08:58 +0800

    Initially build event listener for todo form

--- src/index.js
+++ src/index.js
@@ -40,3 +40,15 @@ document.getElementById("addTodoBtn").addEventListener('click', e => {
   e.target.setAttribute("class", "d-none");
   document.getElementById("todosForm").setAttribute("class", "mt-3");
 });
+
+// Adding a new todo into a project
+document.getElementById("todosForm").addEventListener("submit", e => {
+  e.preventDefault();
+  let inputs = document.getElementsByClassName("todo-form");
+  let title = inputs[0].value;
+  let description = inputs[1].value;
+  let dueDate = new Date(inputs[2].value);
+  let priority = document.getElementsByTagName("select")[0].value;
+  let notes = inputs[3].value;
+  todosController.create(title, description, dueDate, priority, notes, )
+});
