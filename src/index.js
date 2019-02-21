78647139719a5e1cbdeb53af799b29dadaaf5a11
dipto0321 <diptokmk47@gmail.com>
2019-02-21 14:28:42 +0600

    Extract task from submit event listner
    - Extract all task and create new function submitForm
    - Extract create new and update project in createNewProject and
    updateProject function's

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import projectsController from './controllers/projectsController';
-import todosController from "./controllers/todosController";
 import {
   showProjectList,
   appendNewProject,
   showProjectForm
 } from "./controllers/helpers/showProjectList";
-import {
-  showTodoBody
-} from './controllers/helpers/showTodoBody';
 
 
 if (localStorage.getItem("projectsArray") === null) {
@@ -27,20 +23,36 @@ showProjectList();
 // Logic for New Project Submission
 document.getElementById("projectNameForm").addEventListener('submit', e => {
   e.preventDefault();
+  submitForm(e.target);
+});
+
+const submitForm = target => {
   let name = document.getElementById("projectName").value;
   // if data-action == new then this
-  if (e.target.getAttribute("data-action") === "new") {
-    let project = projectsController.create(name);
-    appendNewProject(project);
+  if (target.getAttribute("data-action") === "new") {
+    createNewProject(name);
   } else {
-    let updatedProject = projectsController.update(Number(e.target.getAttribute("data-id")));
-    document.getElementById(`update-proj-${e.target.getAttribute("data-id")}`).setAttribute("class", "btn btn-sm btn-info ml-3");
-    document.getElementById(`projectSpan-${e.target.getAttribute("data-id")}`).innerText = updatedProject.name;
+    updateProject(target);
   }
-  e.target.setAttribute("class", "d-none");
-  e.target.reset();
+  target.setAttribute("class", "d-none");
+  target.reset();
   document.getElementById("addProjBtn").setAttribute("class", "btn btn-sm btn-primary mt-2");
-});
+};
+
+// Create New
+
+const createNewProject = name => {
+  let project = projectsController.create(name);
+  appendNewProject(project);
+};
+
+// Update project
+const updateProject = target => {
+  let updatedProject = projectsController.update(Number(target.getAttribute("data-id")));
+  document.getElementById(`update-proj-${target.getAttribute("data-id")}`).setAttribute("class", "btn btn-sm btn-info ml-3");
+  document.getElementById(`projectSpan-${target.getAttribute("data-id")}`).innerText = updatedProject.name;
+}
+
 // Logic for Add New Project Button
 document.getElementById("addProjBtn").addEventListener("click", e => {
   let action = "new";
