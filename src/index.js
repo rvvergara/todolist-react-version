6e6bae372fd31914799475cf255e7951c5cf6b07
dipto0321 <diptokmk47@gmail.com>
2019-02-21 15:27:17 +0600

    Add basic structure for todos form

--- src/index.js
+++ src/index.js
@@ -28,3 +28,9 @@ document.getElementById("addProjBtn").addEventListener("click", e => {
   let action = "new";
   showProjectForm(e.target, action);
 });
+
+document.getElementById("addTodoBtn").addEventListener('click', e => {
+  e.stopPropagation();
+  e.target.setAttribute("class", "d-none");
+  document.getElementById("todosForm").setAttribute("class", "mt-3");
+});
