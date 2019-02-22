4c20c5b899a1377302cfd3b7bea3aac466cc9482
dipto0321 <diptokmk47@gmail.com>
2019-02-22 12:16:58 +0600

    Refactor showTodoBody

--- src/controllers/helpers/showTodoBody.js
+++ src/controllers/helpers/showTodoBody.js
@@ -1,24 +1,32 @@
 export const showTodoBody = (name) => {
   // Create the parent div 
   let project = JSON.parse(localStorage.getItem(name));
-  // let id = project.id;
   let todoBody = document.getElementById("todoBody");
   todoBody.innerHTML = "";
   // Create the table if there is already a todo in the project
   if (project.todos.length === 0) {
-    let emptyTodoMessage = document.createElement("p");
-    emptyTodoMessage.setAttribute("class", "emptyTodoMessage")
-    emptyTodoMessage.innerText = `No todo items for ${name} yet`;
-    document.getElementsByTagName("table")[0].setAttribute("class", "table table-striped");
-    document.getElementById("todoBody").appendChild(emptyTodoMessage);
+    createEmptyTodoMsg(name);
   } else {
     // Remove class d-none for table
     document.getElementsByTagName("table")[0].setAttribute("class", "table table-striped");
     // Iterate thru each todo and create tr for them
     project.todos.forEach(todo => {
-      // Create tr
-      let tr = document.createElement("tr");
-      let trContent = `
+      createTodoRow(todoBody, todo);
+    });
+  }
+};
+
+const createEmptyTodoMsg = name => {
+  let emptyTodoMessage = document.createElement("p");
+  emptyTodoMessage.setAttribute("class", "emptyTodoMessage")
+  emptyTodoMessage.innerText = `No todo items for ${name} yet`;
+  document.getElementsByTagName("table")[0].setAttribute("class", "table table-striped");
+  document.getElementById("todoBody").appendChild(emptyTodoMessage);
+};
+
+const createTodoRow = (todoBody, todo) => {
+  let tr = document.createElement("tr");
+  let trContent = `
         <td>${todo.title}</td>
         <td>${todo.description}</td>
         <td>${todo.dueDate}</td>
@@ -26,8 +34,6 @@ export const showTodoBody = (name) => {
         <td>${todo.notes}</td>
         <td><button class="btn-sm btn btn-primary">Delete</button></td>
       `;
-      tr.innerHTML = trContent;
-      todoBody.appendChild(tr);
-    });
-  }
+  tr.innerHTML = trContent;
+  todoBody.appendChild(tr);
 };
