059f16e7943a935f03e2ab6ec5b45a70a0129b8c
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-11 15:47:48 +0800

    Add functionality to checkbox in each todo

--- src/js/views/components/TodoItem.js
+++ src/js/views/components/TodoItem.js
@@ -1,23 +1,64 @@
 import React from 'react';
 
-export default (props) => {
-  const {
-    todo
-  } = props;
-  return (
-    <tr>
-      <td>{todo.title}</td>
-      <td>{todo.description}</td>
-      <td>{todo.dueDate}</td>
-      <td>{todo.priority}</td>
-      <td>{todo.notes}</td>
-      <td>
-        <input type="checkbox" value="false" />
-      </td>
-      <td>
-        <button>Update</button>
-        <button>Delete</button>
-      </td>
-    </tr>
-  )
+// export default (props) => {
+//   const {
+//     todo
+//   } = props;
+//   return (
+//     <tr>
+//       <td>{todo.title}</td>
+//       <td>{todo.description}</td>
+//       <td>{todo.dueDate}</td>
+//       <td>{todo.priority}</td>
+//       <td>{todo.notes}</td>
+//       <td>
+//         <input type="checkbox" value="false" />
+//       </td>
+//       <td>
+//         <button>Update</button>
+//         <button>Delete</button>
+//       </td>
+//     </tr>
+//   )
+// }
+
+class TodoItem extends React.Component {
+  state = {
+    done: false,
+  }
+
+  tickProject = () => {
+    this.setState((prevState) => ({
+      done: !prevState.done,
+    }))
+  }
+
+  render() {
+    const {
+      todo
+    } = this.props;
+    const doneClass = this.state.done ? 'strikeout' : '';
+    return (
+      <tr className={doneClass}>
+        <td>{todo.title}</td>
+        <td>{todo.description}</td>
+        <td>{todo.dueDate}</td>
+        <td>{todo.priority}</td>
+        <td>{todo.notes}</td>
+        <td>
+          <input
+          type="checkbox"
+          value="false"
+          onChange={this.tickProject}
+          />
+        </td>
+        <td>
+          <button>Update</button>
+          <button>Delete</button>
+        </td>
+      </tr>
+    )
+  }
 }
+
+export default TodoItem;
