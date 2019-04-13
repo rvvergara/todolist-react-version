45c8233bccb98f248d9e8fd11724dabd6a4745cc
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-13 19:48:32 +0800

    Add proptypes validation to AddTodoBtn

--- src/js/views/components/AddTodoBtn.js
+++ src/js/views/components/AddTodoBtn.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default (props) => {
+const AddTodoBtn = (props) => {
   const {
     handleTodoBtn,
     addTodoMode,
@@ -8,11 +9,18 @@ export default (props) => {
   const displayClass = !addTodoMode ? 'btn btn-primary btn-block' : 'd-none';
   return (
     <button
-      type='button'
+      type="button"
       className={displayClass}
       onClick={handleTodoBtn}
     >
     Add Todo
     </button>
-)
-}
+  );
+};
+
+AddTodoBtn.propTypes = {
+  handleTodoBtn: PropTypes.func.isRequired,
+  addTodoMode: PropTypes.bool.isRequired,
+};
+
+export default AddTodoBtn;
